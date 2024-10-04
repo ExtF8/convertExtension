ad74a03714194ef4e9536b179a2c0290822e9950
ExtF8 <extfield8gmail.com>
2024-10-04 17:50:32 +0300

    Implemented shoe size conversion

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import { CurrencyConverter } from './currency/currencyInput.js';
+import { ShoeSizeConversion } from './shoeSize/shoeSizeInput.js';
 
 const convertCurrencies = new CurrencyConverter();
+const currencyContainer = document.querySelector('.currency-input-container');
+if (currencyContainer) {
+    currencyContainer.addEventListener('input', event =>
+        convertCurrencies.handleInputChanges(event)
+    );
+}
 
-document
-    .querySelector('.currency-input-container')
-    .addEventListener('input', event => convertCurrencies.handleInputChanges(event));
+const convertShoSizes = new ShoeSizeConversion();
+const shoeSizeContainer = document.querySelector('.shoe-input-container');
+if (shoeSizeContainer) {
+    shoeSizeContainer.addEventListener('change', event => {
+        convertShoSizes.handleInputChanges(event);
+    });
+}
