b50795e563aba9377ba9a9a7212c809b2eee469b
ExtF8 <extfield8gmail.com>
2024-10-04 20:33:32 +0300

    Reordered code and added comments

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { MeasuresConverter } from './bodyMeasures/measuresInput.js';
 import { CurrencyConverter } from './currency/currencyInput.js';
 import { ShoeSizeConversion } from './shoeSize/shoeSizeInput.js';
 
+// Instance of the CurrencyConverter class
 const convertCurrencies = new CurrencyConverter();
 const currencyContainer = document.querySelector('.currency-input-container');
 if (currencyContainer) {
@@ -10,18 +11,20 @@ if (currencyContainer) {
     );
 }
 
-const convertShoSizes = new ShoeSizeConversion();
-const shoeSizeContainer = document.querySelector('.shoe-input-container');
-if (shoeSizeContainer) {
-    shoeSizeContainer.addEventListener('change', event => {
-        convertShoSizes.handleInputChanges(event);
-    });
-}
-
+// Instance of the MeasuresConverter class for body measurements
 const convertBodyMeasures = new MeasuresConverter();
 const bodyMeasuresContainer = document.querySelector('.measures-input-container');
 if (bodyMeasuresContainer) {
     bodyMeasuresContainer.addEventListener('change', event => {
         convertBodyMeasures.handleInputChanges(event);
+    });
+}
+
+// Instance of the ShoeSizeConversion class for shoe size conversions
+const convertShoSizes = new ShoeSizeConversion();
+const shoeSizeContainer = document.querySelector('.shoe-input-container');
+if (shoeSizeContainer) {
+    shoeSizeContainer.addEventListener('change', event => {
+        convertShoSizes.handleInputChanges(event);
     });
 }
